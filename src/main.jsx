dccd13c8b746c23f4cddc850da1ed5a729551624
Genesis Longart <gnsslr@gmail.com>
2023-09-04 15:23:23 -0300

    Home landing page

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
 
 import '@fortawesome/fontawesome-free/css/all.min.css'
-
+import './style.css'
 import App from './App'
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,23 +5,27 @@ const Home = () => {
     const { store, actions } = useContext(Context);
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h3><i className="fa-solid fa-home fa-1x"></i> Home</h3>
-                </div>
+        <div className='d-flex flex-column align-items-center'>
+            <img src="./public/img/wave.png" alt="" className='container-fluid m-0' />
+            <img src="./public/img/logo.png" alt="" className='w-25' />
+            
+            <div className="col-4 d-flex flex-column justify-content-center mt-5">
+                <label for="username" className="form-label text-white">Usuario:</label>
+                <input type="email" className="form-control text-white" id="username"></input>
+                <label for="password" className="form-label text-white">Contraseña:</label>
+                <input type="email" className="form-control text-white" id="password"></input>
+                <p className='text-center mb-5 text-white'>¿Has olvidado tu contraseña?</p>
             </div>
-            <div className="row">
-                <div className="col-md-12">
-                    <span className='m-2 text-danger'>
-                        {store?.name}
-                    </span>
+            <div className='d-flex flex-column'>
+            <button type="button" className="btn btn-primary mb-2 ">Iniciar Sesión</button>
+            <button type="button" className="btn btn-secondary mb-2">Crear nueva cuenta</button>
+            <button type="button" className="btn btn-light mb-2"><span><img src="./public/img/logogoogle.png" height={"30px"}/></span>Iniciar sesión con Google</button>
+            </div>
 
-                    <button className="btn btn-info" onClick={actions.greetings}>
-                        Saludar
-                    </button>
-                </div>
-            </div>
+            <footer className='container-fluid bg-primary p-3 text-center'>
+                <p>Copyright © Jermain Chacón, Ryan Daniels y Genesis Longart</p>
+            </footer>
+
         </div>
     )
 }
